5f288163ef097b73e46cbf0ee9582ce77a0c98ed
KellyEgesa <kelly.egesa@gmail.com>
2021-03-16 14:06:48 +0300

    Finished basic repository activity layout

---
+++ app/src/main/java/com/mamawaestate/githubissuetracker/UserInterface/RepositoryActivity.java
@@ -0,0 +1,16 @@
+package com.mamawaestate.githubissuetracker.UserInterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mamawaestate.githubissuetracker.R;
+
+public class RepositoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository);
+    }
+}
