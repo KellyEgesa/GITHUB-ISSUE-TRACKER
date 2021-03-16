d63065c48a6e9a4bcd08035d8687591745a0c939
KellyEgesa <kelly.egesa@gmail.com>
2021-03-16 12:42:23 +0300

    Finished basic user name layout

---
+++ app/src/main/java/com/mamawaestate/githubissuetracker/UserInterface/UserNameActivity.java
@@ -0,0 +1,16 @@
+package com.mamawaestate.githubissuetracker.UserInterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mamawaestate.githubissuetracker.R;
+
+public class UserNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_name);
+    }
+}
