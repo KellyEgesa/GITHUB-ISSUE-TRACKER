a7352fc3d977e1470ef9978667d3f6985d11a29b
KellyEgesa <kelly.egesa@gmail.com>
2021-03-16 12:11:53 +0300

    Added main activity

--- app/src/main/java/com/mamawaestate/githubissuetracker/UserInterface/MainActivity.java
+++ app/src/main/java/com/mamawaestate/githubissuetracker/UserInterface/MainActivity.java
@@ -1,8 +1,10 @@
-package com.mamawaestate.githubissuetracker;
+package com.mamawaestate.githubissuetracker.UserInterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.mamawaestate.githubissuetracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
