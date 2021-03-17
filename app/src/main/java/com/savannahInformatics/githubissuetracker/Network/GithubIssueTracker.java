e5e6cca4bc5660a06e9a77d24a5cb291f17ab51b
KellyEgesa <kelly.egesa@gmail.com>
2021-03-17 19:31:02 +0300

    Added githubUserrepo and githubUserDetails models

--- app/src/main/java/com/savannahInformatics/githubissuetracker/Network/GithubIssueTracker.java
+++ app/src/main/java/com/savannahInformatics/githubissuetracker/Network/GithubIssueTracker.java
@@ -1,12 +1,14 @@
 package com.savannahInformatics.githubissuetracker.Network;
 
+import com.savannahInformatics.githubissuetracker.Models.GitHubUserDetails;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface GithubIssueTracker {
     @GET("users/{username}")
-    Call checkUser(@Path("username") String username);
+    Call<GitHubUserDetails> checkUser(@Path("username") String username);
 
     @GET("users/{username}/repos")
     Call getUserRepos(@Path("username") String username);
--- app/src/main/java/com/savannahInformatics/githubissuetracker/UserInterface/UserNameActivity.java
+++ app/src/main/java/com/savannahInformatics/githubissuetracker/UserInterface/UserNameActivity.java
@@ -8,11 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.savannahInformatics.githubissuetracker.Models.GitHubUserDetails;
+import com.savannahInformatics.githubissuetracker.Network.GitHubClient;
+import com.savannahInformatics.githubissuetracker.Network.GithubIssueTracker;
 import com.savannahInformatics.githubissuetracker.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserNameActivity extends AppCompatActivity {
     @BindView(R.id.proceedButton)
@@ -37,12 +44,27 @@ public class UserNameActivity extends AppCompatActivity {
         });
     }
 
-    private void searchGitHubUserName(){
+    private void searchGitHubUserName() {
         String username = mGitHubUserName.getText().toString().trim();
 
-        if(!isUserNameValid(username)){
+        if (!isUserNameValid(username)) {
             return;
         }
+
+        GithubIssueTracker client = GitHubClient.urlRequest();
+        Call<GitHubUserDetails> call = client.checkUser(username);
+        call.enqueue(new Callback<GitHubUserDetails>() {
+            @Override
+            public void onResponse(Call<GitHubUserDetails> call, Response<GitHubUserDetails> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<GitHubUserDetails> call, Throwable t) {
+                Toast.makeText(UserNameActivity.this, "User not Found", Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         Intent intent = new Intent(UserNameActivity.this, RepositoryActivity.class);
         intent.putExtra("githubUserName", username);
@@ -58,11 +80,28 @@ public class UserNameActivity extends AppCompatActivity {
         return true;
     }
 
-    private void loadingScreen(){
+    private void loadingScreen() {
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Searching");
         progressDialog.setMessage("Searching for user with the given username");
         progressDialog.setCancelable(false);
+    }
+
+    private void getRepos(String username){
+        GithubIssueTracker client = GitHubClient.urlRequest();
+        Call<GitHubUserDetails> call = client.getUserRepos(username);
+
+        call.enqueue(new Callback<GitHubUserDetails>() {
+            @Override
+            public void onResponse(Call<GitHubUserDetails> call, Response<GitHubUserDetails> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<GitHubUserDetails> call, Throwable t) {
+
+            }
+        });
     }
 
 
