482563c12091b2fb578bf4a2a57743d3f774a970
KellyEgesa <kelly.egesa@gmail.com>
2021-03-17 16:15:19 +0300

    Issue Tracker

---
+++ app/src/main/java/com/mamawaestate/githubissuetracker/Network/GitHubClient.java
@@ -0,0 +1,4 @@
+package com.mamawaestate.githubissuetracker.Network;
+
+public class GitHubClient {
+}
--- app/src/main/java/com/mamawaestate/githubissuetracker/Services/Main.java
+++
@@ -1,4 +0,0 @@
-package com.mamawaestate.githubissuetracker.Services;
-
-public class Main {
-}
